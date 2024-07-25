776cd841e00634a1fd4da1c6bb4f67956f63ba77
gelutz <gabriel.lutz5@gmail.com>
2024-07-24 21:38:25 -0300

    class-components

---
+++ class-components/PostsList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class PostsList extends React.Component {
+  // método principal que renderiza o componente
+  render() {
+    return (
+      <div>
+        <h1>Posts List</h1>
+      </div>
+    );
+  }
+}
