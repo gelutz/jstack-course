101e88c2d868539b8e8f04abe4eccfe85b47ff42
gelutz <gabriel.lutz5@gmail.com>
2024-07-27 02:46:32 -0300

    feat: routes e papapa

--- jstack-app/src/Routes.js
+++ jstack-app/src/Routes.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
 
 export default function Router() {
+  const location = useLocation();
+
+  // const transitions = useTransition(location, {
+
+  //   from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+  //   enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+  //   leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+  // });
   return (
     <BrowserRouter>
       <Routes>
