053738ff7f1f8d487cbdebfd8bc89c96c5732b64
gelutz <gabriel.lutz5@gmail.com>
2024-07-29 21:43:41 -0300

    feat: starting info section - aula 7

--- contact-app/web/src/components/ContactsList/index.jsx
+++ contact-app/web/src/components/ContactsList/index.jsx
@@ -1,6 +1,7 @@
-import { Container, Header } from "./styles";
+import { Card, Container, Header, ListContainer } from "./styles";
 
 import React from "react";
+import arrow from "../../assets/images/arrow.svg";
 
 export default function ContactsList() {
   return (
@@ -9,6 +10,21 @@ export default function ContactsList() {
         <h3>3 Contatos</h3>
         <a href="#">Novo Contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info"></div>
+        </Card>
+        <Card></Card>
+        <Card></Card>
+      </ListContainer>
     </Container>
   );
 }
--- contact-app/web/src/components/ContactsList/styles.js
+++ contact-app/web/src/components/ContactsList/styles.js
@@ -20,8 +20,6 @@ export const Header = styled.header`
     text-decoration: none;
     font-weight: bold;
     border: 2px solid ${({ theme }) => theme.colors.primary.main};
-    border-radius: 4px;
-    padding: 8px 16px;
 
     transition: all 0.3s cubic-bezier(0.6, -0.28, 0.735, 0.045);
     /* transition: all 0.3s ease-in-out; */
@@ -31,4 +29,37 @@ export const Header = styled.header`
       color: #fff;
     }
   }
+`;
+
+export const ListContainer = styled.div`
+  border-top: 2px solid ${({ theme }) => theme.colors.primary.lighter};
+  margin-top: 24px;
+  padding-top: 24px;
+
+  header {
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+    }
+
+    span {
+      color: ${({ theme }) => theme.colors.primary.main};
+      margin-right: 8px;
+      font-weight: bold;
+    }
+  }
+`;
+
+export const Card = styled.div`
+  margin-bottom: 8px;
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+  padding: 16px;
+  border-radius: 4px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
