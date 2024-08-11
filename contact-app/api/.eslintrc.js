ce77a98965c16396a467c39f6393700e210e67e9
gelutz <gabriel.lutz5@gmail.com>
2024-08-11 02:33:55 -0300

    feat: docker-compose ok

--- contact-app/api/.eslintrc.js
+++ contact-app/api/.eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["airbnb-base"],
-  parserOptions: {
-    ecmaVersion: 13,
-  },
-  rules: {
-    "class-methods-use-this": "off",
-    "no-promise-executor-return": "off",
-    "consistent-return": "off",
-    camelcase: "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
-  },
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["eslint:recommended"],
+	parserOptions: {
+		ecmaVersion: 13,
+	},
+	rules: {
+		"class-methods-use-this": "off",
+		"no-promise-executor-return": "off",
+		"consistent-return": "off",
+		camelcase: "off",
+		"no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+	},
 };
