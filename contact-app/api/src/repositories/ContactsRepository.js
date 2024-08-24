5f8602a8118fc24067286c128a028decb4038b19
gelutz <gabriel.lutz5@gmail.com>
2024-08-24 10:35:15 -0300

    feat: removendo pasta app/ desnecessária

--- contact-app/api/src/repositories/ContactsRepository.js
+++ contact-app/api/src/repositories/ContactsRepository.js
@@ -1,4 +1,4 @@
-const db = require("../../database");
+const db = require("../database");
 
 class ContactsRepository {
 	async findAll(orderBy = "ASC") {
--- contact-app/api/src/routes.js
+++ contact-app/api/src/routes.js
@@ -1,17 +1,17 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
-const ContactController = require('./app/controllers/ContactController');
-const CategoryController = require('./app/controllers/CategoryController');
+const ContactController = require("./controllers/ContactController");
+const CategoryController = require("./controllers/CategoryController");
 
 const router = Router();
 
-router.get('/contacts', ContactController.index);
-router.get('/contacts/:id', ContactController.show);
-router.delete('/contacts/:id', ContactController.delete);
-router.post('/contacts', ContactController.store);
-router.put('/contacts/:id', ContactController.update);
+router.get("/contacts", ContactController.index);
+router.get("/contacts/:id", ContactController.show);
+router.delete("/contacts/:id", ContactController.delete);
+router.post("/contacts", ContactController.store);
+router.put("/contacts/:id", ContactController.update);
 
-router.get('/categories', CategoryController.index);
-router.post('/categories', CategoryController.store);
+router.get("/categories", CategoryController.index);
+router.post("/categories", CategoryController.store);
 
 module.exports = router;
